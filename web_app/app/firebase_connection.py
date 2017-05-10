99881418fa06a8cbfe30fe6cd6c06bb782574b03
Kelly Ochikubo <Kelly@Kellys-MacBook-Pro.local>
2017-05-10 16:12:59 -0700

    without credentials

--- web_app/app/firebase_connection.py
+++ web_app/app/firebase_connection.py
@@ -2,16 +2,9 @@ import json
 
 import pyrebase
 
+
 # with open('secret.json') as f:
 #     data = json.load(f)
-
-config = {"apiKey": "AIzaSyCw6X5uPTCS51Lh4z1-NS4BBCJAJYa18pQ",
-          "authDomain": "hops-66a78.firebaseapp.com",
-          "databaseURL": "https://hops-66a78.firebaseio.com",
-          "storageBucket": "hops-66a78.appspot.com"}
-
-email = "kelly.ochikubo@gmail.com"
-password = "hopsrecommender"
 
 
 def connect_to_database():
