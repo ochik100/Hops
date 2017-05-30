10a73ac013c100687bfa92935eea441c88faa0dd
Kelly Ochikubo <Kelly@Kellys-MacBook-Pro.local>
2017-05-29 22:15:57 -0700

    persist df tokens

--- recommender/reviews.py
+++ recommender/reviews.py
@@ -62,5 +62,6 @@ def group_tokens_by_beer(df_tokens):
 def get_beer_reviews_dataframe(df_reviews):
     lemmatize_review_udf = udf(lambda x: preprocess_review_text(x), ArrayType(StringType()))
     df_tokens = df_reviews.withColumn("tokens", lemmatize_review_udf('text'))
+    df_tokens.persist(ps.StorageLevel.MEMORY_AND_DISK)
     df_beer_reviews = group_tokens_by_beer(df_tokens)
     return df_beer_reviews
