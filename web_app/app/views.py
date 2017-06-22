dee3dde492ff912c316a7a08e950fe2681222980
Kelly Ochikubo <Kelly@Kellys-MacBook-Pro.local>
2017-06-21 17:27:35 -0700

    new pdf & web ui look

--- web_app/app/views.py
+++ web_app/app/views.py
@@ -12,24 +12,34 @@ token, db = connect_to_database()
 
 @app.route('/results', methods=['GET', 'POST'])
 def index():
-
-    # recommendations = np.load(file=os.path.join(APP_STATIC, 'data/five_most_similar_beers.npy'))
-    # print recommendations
     results = []
     beer_name = ""
     if request.method == "POST":
         beer_index = request.form.get('index')
         print "BEER INDEX - ", beer_index
         beer = db.child('beers').child(beer_index).get(token).val()
-        beer_name = [beer['beer_name'], beer['brewery_name']]
+        beer_name = [beer['beer_name'], beer['brewery_name'], beer['top1'], beer['top2'], beer['top3'], beer['top4'], beer['top5'],
+                     beer['top6'], beer['top7'], beer['beer_style']]
         indexes = np.array([beer['first'], beer['second'], beer[
                            'third'], beer['fourth'], beer['fifth']])
         for idx in indexes:
-            details = db.child('beer_names').child(idx).get(token).val()
-            output = (details['beer_name'].encode('utf-8'),
-                      details['brewery_name'].encode('utf-8'))
+            details = db.child('beers').child(idx).get(token).val()
+            output = unicode_helper((details['beer_name'],
+                                     details['brewery_name'],
+                                     details['beer_style'],
+                                     details['top1'],
+                                     details['top2'],
+                                     details['top3'],
+                                     details['top4'],
+                                     details['top5'],
+                                     details['top6'],
+                                     details['top7']))
             results.append(output)
     return render_template("index.html", beer_name=beer_name, results=results)
+
+
+def unicode_helper(output):
+    return map(lambda x: x.encode('utf-8', 'ignore').decode('utf-8'), output)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -46,8 +56,3 @@ def about():
 @app.route('/beer_styles')
 def beer_styles():
     return render_template("beer_styles.html")
-
-
-@app.route('/test', methods=['GET'])
-def test():
-    return render_template('test.html')
